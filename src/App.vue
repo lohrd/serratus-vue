c62233c45635ef53a1173add72fa14a222b49e16
lohrd <57124913+lohrd@users.noreply.github.com>
2020-05-13 13:28:04 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Navbar />
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import './assets/css/tailwind.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home,
+    About
   }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
